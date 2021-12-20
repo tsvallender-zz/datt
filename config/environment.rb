e0dd18c4177f1b8239ce1603cde0a55c1e1345e4
Trevor Vallender <trevor@tsvallender.co.uk>
2021-12-20 20:49:23 +0000

    Using Sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey',
-  :password => <%= ENV['SENDGRID_API_KEY'] %>,
+  :password => ENV['SENDGRID_API_KEY'],
   :domain => 'datt.tsvallender.co.uk',
   :address => 'smtp.sendgrid.net',
   :port => 587,
