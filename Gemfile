f526be62ea864d48ca100a0ae334b47cffdb6e92
Trevor Vallender <trevor@tsvallender.co.uk>
2022-01-17 10:49:41 +0000

    Added test for todo rendering

--- Gemfile
+++ Gemfile
@@ -36,4 +36,5 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
   gem "database_cleaner"
+  gem "launchy"
 end
---
+++ spec/features/user_views_todos_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'User views their todos' do
+  scenario "successfully" do
+    @user ||= FactoryBot.create :user
+    todo = @user.notes.build(
+      title: 'My todo', content: 'My todo',
+      type: 'Todo', due_date: Date.tomorrow)
+    todo.save!
+    sign_in @user
+    visit todo_path(todo.id)
+    expect(page).to have_css 'h2', text: 'My todo'
+    expect(page).to have_css '.note .content', text: 'My todo'
+    expect(page).to have_css '.note .due_date'
+  end
+end
